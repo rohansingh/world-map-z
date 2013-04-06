86421062bfd2a7d7d05d138dbaebb2917a09d8e8
Rohan Singh <rohan@spotify.com>
2013-04-06 20:50:37 +0200

    Project the map better
    Use a Miller projection and scale and translate the map for better viewing.

--- public/main.js
+++ public/main.js
@@ -1,9 +1,19 @@
+var MAP_WIDTH = 1132;
+var MAP_HEIGHT = 554;
+var PROJECTION_SCALE = 180;
+
 var svg = d3.select("body").append("svg")
-  .attr("width", 960)
-  .attr("height", 1160);
+  .attr("width", MAP_WIDTH)
+  .attr("height", MAP_HEIGHT);
   
 d3.json("world.json", function(error, world) {
+  var projection = d3.geo.miller()
+    .scale(PROJECTION_SCALE)
+    .translate([MAP_WIDTH / 2, MAP_HEIGHT / 2 + 80]);
+
+  var path = d3.geo.path().projection(projection);
+
   svg.append("path")
     .datum(topojson.object(world, world.objects.subunits))
-    .attr("d", d3.geo.path().projection(d3.geo.mercator()));
+    .attr("d", path);
 });
